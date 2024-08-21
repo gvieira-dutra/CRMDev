f5054e93947d49d4f77d8915be6c864b572a42bc
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-08-20 23:02:48 -0400

    modified view models

--- CRMDev.Application/ViewModels/OpportunityDetailVM.cs
+++ CRMDev.Application/ViewModels/OpportunityDetailVM.cs
@@ -1,8 +1,9 @@
-﻿using CRMDev.Core.Enums;
+﻿using CRMDev.Core.Entities;
+using CRMDev.Core.Enums;
 
 namespace CRMDev.Application.ViewModels
 {
-    public class OpportunityDetailVM(string title, string description, DateTime deliveryEstimate, decimal estimative, string scope, bool supportIncluded, Status status, ReasonForLostDeal reasonForLostDeal, Stage stage, int contactId)
+    public class OpportunityDetailVM(string title, string description, DateTime deliveryEstimate, decimal estimative, string scope, bool supportIncluded, Status status, ReasonForLostDeal reasonForLostDeal, List<Stage> stages, int contactId, string currStage, string currTask, string currTaskDescrip)
     {
         public string Title { get; private set; } = title;
         public string Description { get; private set; } = description;
@@ -12,7 +13,10 @@ namespace CRMDev.Application.ViewModels
         public bool SupportIncluded { get; private set; } = supportIncluded;
         public Status Status { get; private set; } = status;
         public ReasonForLostDeal ReasonForLostDeal { get; private set; } = reasonForLostDeal;
-        public Stage Stage { get; private set; } = stage;
         public int ContactId { get; private set; } = contactId;
+        public string CurrStage { get; private set; } = currStage;
+        public string CurrTask { get; private set; } = currTask;
+        public string CurrTaskDescrip { get; private set; } = currTaskDescrip;
+        public List<Stage> Stages { get; private set; } = stages;
     }
 }
--- CRMDev.Application/ViewModels/OpportunityVM.cs
+++ CRMDev.Application/ViewModels/OpportunityVM.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CRMDev.Application.ViewModels
+﻿namespace CRMDev.Application.ViewModels
 {
     public class OpportunityVM(int id, string title, string description, decimal estimative)
     {
