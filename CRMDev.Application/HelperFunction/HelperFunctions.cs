f3131f833c842a1bd4de3339185b65c90dc9e4c4
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-08-30 15:57:26 -0400

    helper function to be used by different functions

---
+++ CRMDev.Application/HelperFunction/HelperFunctions.cs
@@ -0,0 +1,58 @@
+﻿using CRMDev.Application.ViewModels;
+using CRMDev.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMDev.Application.HelperFunction
+{
+    public class HelperFunctions
+    {
+        public async Task<OpportunityDetailVM> CreateOpportunityDetailVM(CRMDevDbContext _dbContext, int id)
+        {
+            var opportunity = await _dbContext.Opportunities
+                .Include(s => s.Stages)
+                    .ThenInclude(t => t.Tasks)
+                .Include(c => c.Contact)
+                .SingleOrDefaultAsync(o => o.Id == id);
+
+            var opportunityVM = new OpportunityDetailVM(
+                opportunity.Title,
+                opportunity.Description,
+                opportunity.DeliveryEstimate,
+                opportunity.Estimative,
+                opportunity.Scope,
+                opportunity.SupportIncluded,
+                opportunity.Status,
+                opportunity.ReasonForLostDeal,
+                opportunity.Stages,
+                opportunity.ContactId,
+                opportunity.Stages[opportunity.CurrentStageTracker].Name.ToString() ?? "N/A",
+                opportunity.Stages[opportunity.CurrentStageTracker].Tasks[opportunity.CurrentTaskTracker].Name.ToString() ?? "N/A",
+                opportunity.Stages[opportunity.CurrentStageTracker].Tasks[opportunity.CurrentTaskTracker].Description.ToString() ?? "N/A"
+
+            );
+
+            return opportunityVM;
+        }
+
+        public async Task<ContactDetailVM> CreateContactDetailVM(CRMDevDbContext _dbContext, int id)
+        {
+            var contactVM = await _dbContext.Contacts
+               .Where(c => c.Id == id)
+               .Select(c => new ContactDetailVM(
+                   c.Name,
+                   c.Email,
+                   c.Phone,
+                   c.CellPhone,
+                   c.FieldOrIndustry.FieldName,
+                   c.Position,
+                   c.Address,
+                   c.IsActive.ToString(),
+                   c.ContactNotesSummary ?? "N/A",
+                   c.Notes)
+               )
+               .FirstOrDefaultAsync();
+
+            return contactVM;
+        }
+    }
+}
