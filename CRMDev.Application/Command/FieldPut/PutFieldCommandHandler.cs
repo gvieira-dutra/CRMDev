755db4bf57db24c14f48148d21591cd48014b3e4
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-08-31 16:04:00 -0400

    CQRS pattern in field entity

---
+++ CRMDev.Application/Command/FieldPut/PutFieldCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using CRMDev.Application.HelperFunction;
+using CRMDev.Application.ViewModels;
+using CRMDev.Core.Entities;
+using CRMDev.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMDev.Application.Command.FieldPut
+{
+    public class PutFieldCommandHandler : IRequestHandler<PutFieldCommand, FieldOrIndustryVM>
+    {
+        private readonly CRMDevDbContext _dbContext;
+        private readonly HelperFunctions _helper;
+        public PutFieldCommandHandler(CRMDevDbContext dbContext, HelperFunctions helper)
+        {
+            _dbContext = dbContext;
+            _helper = helper;
+        }
+        public async Task<FieldOrIndustryVM> Handle(PutFieldCommand request, CancellationToken cancellationToken)
+        {
+            var fieldToBeEdited = await _dbContext.FieldOrIndustries
+                .Where(f => f.Id == request.Id)
+                .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+            var fieldNewInfo = new FieldOrIndustry
+                (
+                    request.FieldName,
+                    request.Description
+                );
+
+            fieldToBeEdited?.EditField(fieldNewInfo);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return await HelperFunctions.CreateFieldOrIndustryVM(_dbContext, request.Id);
+        }
+    }
+}
