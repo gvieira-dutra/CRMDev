ba9597e63451464004a178ec0ca98d296b3aef8f
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-08-31 16:03:19 -0400

    CQRS pattern in field entity

---
+++ CRMDev.Application/Command/FieldPut/PutFieldCommand.cs
@@ -0,0 +1,12 @@
+﻿using CRMDev.Application.ViewModels;
+using MediatR;
+
+namespace CRMDev.Application.Command.FieldPut
+{
+    public class PutFieldCommand : IRequest<FieldOrIndustryVM>
+    {
+        public int Id { get; set; }
+        public string FieldName { get; set; }
+        public string Description { get; set; }
+    }
+}
