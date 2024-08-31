ba4ddaeb85d6989ebdf9c387ddb3c701cf208dfb
Gleison <126490844+gvieira-dutra@users.noreply.github.com>
2024-08-31 16:03:01 -0400

    CQRS pattern in contact entity

---
+++ CRMDev.Application/Query/ContactGetAll/GetAllContactQuery.cs
@@ -0,0 +1,10 @@
+﻿using CRMDev.Application.ViewModels;
+using MediatR;
+
+namespace CRMDev.Application.Query.ContactGetAll
+{
+    public class GetAllContactQuery : IRequest<List<ContactVM>>
+    {
+        public string Query { get; set; }
+    }
+}
---
+++ CRMDev.Application/Query/ContactGetAll/GetAllContactQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CRMDev.Application.HelperFunction;
+using CRMDev.Application.ViewModels;
+using CRMDev.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMDev.Application.Query.ContactGetAll
+{
+    public class GetAllContactQueryHandler : IRequestHandler<GetAllContactQuery, List<ContactVM>>
+    {
+        private readonly CRMDevDbContext _dbContext;
+        private readonly HelperFunctions _helper;
+        public GetAllContactQueryHandler(CRMDevDbContext dbContext, HelperFunctions helper)
+        {
+            _dbContext = dbContext;
+            _helper = helper;
+        }
+        public async Task<List<ContactVM>> Handle(GetAllContactQuery request, CancellationToken cancellationToken)
+        {
+            var contacts = await _dbContext.Contacts
+                .Where(c => c.IsActive == true)
+                .Select(c => new ContactVM(c.Name, c.Email, c.Phone, c.FieldOrIndustry.FieldName, c.Position))
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            return contacts;
+        }
+    }
+}
